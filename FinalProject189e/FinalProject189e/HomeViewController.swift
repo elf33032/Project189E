e526453a1a7709971ee123a95e5b153746c33d19
Yizeng <yizzhou@ucdavis.edu>
2021-02-28 00:40:34 -0800

    add

--- FinalProject189e/FinalProject189e/HomeViewController.swift
+++ FinalProject189e/FinalProject189e/HomeViewController.swift
@@ -6,14 +6,31 @@
 //
 
 import UIKit
+import MapKit
 
 class HomeViewController: UIViewController {
 
+    
+    @IBOutlet private var mapView: MKMapView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        let initialLocation = CLLocation(latitude: 38.537, longitude: -121.754)
+        mapView.centerToLocation(initialLocation)
     }
-    
+}
+
+private extension MKMapView {
+  func centerToLocation(
+    _ location: CLLocation,
+    regionRadius: CLLocationDistance = 1000
+  ) {
+    let coordinateRegion = MKCoordinateRegion(
+      center: location.coordinate,
+      latitudinalMeters: regionRadius,
+      longitudinalMeters: regionRadius)
+    setRegion(coordinateRegion, animated: true)
+  }
     
     
 
